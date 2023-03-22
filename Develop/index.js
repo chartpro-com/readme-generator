68da67fc0ebb2d6c08daceed4312278d4255b161
Matt Fleming <122564640+chartpro-com@users.noreply.github.com>
2023-03-22 10:14:42 -0400

    create working mvp of project

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,45 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const generateMD = ({title, description}) => 
+`
+    #${title}
+
+    ##${description}
+`;
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe your project:',
+  },
+])
+
+.then((response) => {
+  const md = generateMD(response);
+  fs.writeFile('READEME.md', md, (err) => {
+    if (err) throw err;
+    console.log('README created!');
+  });
+});
