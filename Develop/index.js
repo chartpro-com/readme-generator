b57c2f66d528671054f5ee7b2218f234d790474a
Matt Fleming <122564640+chartpro-com@users.noreply.github.com>
2023-03-25 10:54:28 -0400

    create functions as required in the source code

--- Develop/index.js
+++ Develop/index.js
@@ -1,40 +1,75 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
-
-// const fileName = "READEME.md"
+const fs = require('fs');
+const { generateMarkdown, renderLicenseBadge } = require('./utils/generateMarkdown');
 
 const questions = [
-{
-  type: 'input',
-  name: 'title',
-  message: 'What is the title of your project?',
-},
-{
-  type: 'input',
-  name: 'description',
-  message: 'Describe your project:',
-},
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How should your project be used?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Who contributed?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'How do you test?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Which license does you use?',
+    choices: [
+      'MIT',
+      'GNU GPLv3',
+      'Apache License 2.0',
+      'ISC License',
+      'None',
+    ],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
 ];
 
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log('README file created!')
-  );
-}
+inquirer.prompt(questions)
+  .then(data => {
+    data.badge = renderLicenseBadge(data.license);
+    const markdown = generateMarkdown(data);
 
-function init() {
-  inquirer.prompt(questions).then((answers) => {
-    const readme = `
-# ${answers.title}
-
-## Description
-
-${answers.description}
-
-
-    `;
-    writeToFile('README.md', readme);
+    fs.writeFile('README.md', markdown, err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('README.md generated!');
+      }
+    });
+  })
+  .catch(err => {
+    console.log(err);
   });
-}
-
-init();
